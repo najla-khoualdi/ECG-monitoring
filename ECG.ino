65af44b817744cabdcfade86730424df6291e586
najla-khoualdi <80226616+najla-khoualdi@users.noreply.github.com>
2022-10-01 14:01:52 +0100

    Add files via upload

---
+++ ECG.ino
@@ -0,0 +1,54 @@
+#include <eHealth.h>
+#include <eHealthDisplay.h>
+
+#ifndef prog_unit8_t
+#define prog_unit8_t const unit8_t
+/*#include <eHealthDisplay.h>
+
+/*  
+ *  eHealth sensor platform for Arduino and Raspberry from Cooking-hacks.
+ *  
+ *  Copyright (C) Libelium Comunicaciones Distribuidas S.L. 
+ *  http://www.libelium.com 
+ *  
+ *  This program is free software: you can redistribute it and/or modify 
+ *  it under the terms of the GNU General Public License as published by 
+ *  the Free Software Foundation, either version 3 of the License, or 
+ *  (at your option) any later version. 
+ *  a
+ *  This program is distributed in the hope that it will be useful, 
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License 
+ *  along with this program.  If not, see http://www.gnu.org/licenses/. 
+ *  
+ *  Version:           2.0
+ *  Design:            David Gascón 
+ *  Implementation:    Luis Martin & Ahmad Saad
+ 
+
+#include < eHealth.h >
+*/
+
+
+
+// The setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+}
+
+// The loop routine runs over and over again forever:
+void loop() {
+
+  float ECG = eHealth.getECG();
+
+  Serial.print("ECG value :  ");
+  Serial.print(ECG, 2);
+  Serial.print(" V");
+  Serial.println("");
+
+  delay(1);  // wait for a millisecond
+}
+#endif
